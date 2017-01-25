59eb852914fbfa4662ce3fa0306207bc1cbdbb53
Villy Siu <villysiu@gmail.com>
2017-01-25 00:16:15 +0000

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,20 @@
 'use strict';
+var recipe = {};
+function addIngredient(recipe, ingredient, amount){
+  recipe[ingredient] = amount;
+  return recipe;
+}
+function removeIngredient (recipe, ingredient){
+  delete recipe[ingredient];
+  return recipe;
+}
+function updateIngredient(recipe, ingredient, amount){
+  recipe[ingredient] = amount;
+  return recipe;
+}
+function readRecipe(recipe){
+  for (var i in recipe) {
+    console.log("this recipe calls for " + recipe[i] + " of " + i)
+  }
+  
+}
